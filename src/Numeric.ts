547b1390645c49df7298baef022950af63c6c9d1
Nate Buckareff <nate.buckareff@gmail.com>
2019-12-09 15:35:30 -0500

    Fixes import start issue

--- src/Numeric.ts
+++ src/Numeric.ts
@@ -1,4 +1,5 @@
-import * as ok from './index';
+import { PrimitiveCodec, Number as Num } from './Primitive';
+import { Ok, Err } from './Result';
 
 // XXX TODO should use something like newtype-ts
 export type IntegerType = number & { readonly __tag: unique symbol };
@@ -6,15 +7,15 @@ export type IntegerType = number & { readonly __tag: unique symbol };
 // XXX TODO serializing should not result in an unknown; instead we should add
 // an additional type paramter to Codec so that the output of `serialize` does
 // not have to equal the input of `parse`
-const _Integer = new ok.PrimitiveCodec<unknown, IntegerType>(
+const _Integer = new PrimitiveCodec<unknown, IntegerType>(
     'Integer',
     i => {
-        return ok.Number.parse(i).pipe(x =>
+        return Num.parse(i).pipe(x =>
             Number.isSafeInteger(x)
-                ? ok.Ok(x)
-                : ok.Err(new Error('Expected safe integer')),
+                ? Ok(x)
+                : Err(new Error('Expected safe integer')),
         );
     },
-    ok.Ok,
+    Ok,
 );
 export { _Integer as Integer };
