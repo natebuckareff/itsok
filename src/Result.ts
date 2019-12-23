dee4379682bfb5547a9679db9922b66c3b7eef27
Nate Buckareff <nate.buckareff@gmail.com>
2019-12-23 14:35:08 -0500

    Added Result.join and refactored Result

--- src/Result.ts
+++ src/Result.ts
@@ -15,11 +15,30 @@ export class Result<T, E> {
         }
     }
 
+    ok(): Result<T, any> {
+        return this;
+    }
+
+    err(): Result<any, E> {
+        return this;
+    }
+
     unwrap() {
         if (this.isError) {
             throw this.error;
         }
         return this.success;
+    }
+
+    static join<T, E>(results: Result<T, E>[]): Result<T[], E> {
+        const j = [];
+        for (const r of results) {
+            if (r.isError) {
+                return r.err();
+            }
+            j.push(r.success);
+        }
+        return Ok(j);
     }
 }
 
@@ -27,13 +46,14 @@ export function Ok<T>(success: T): Result<T, any> {
     return new Result(success, null, false);
 }
 
-export function Err<T>(error: T): Result<any, T> {
-    return new Result(null, error, true);
+function Err(message: string): Result<any, Error>;
+function Err<E extends Error>(error: E): Result<any, E>;
+function Err(x: any) {
+    return new Result(null, typeof x === 'string' ? new Error(x) : x, true);
 }
+export { Err };
 
-export function Try<TSuccess, TError>(
-    cb: () => TSuccess,
-): Result<TSuccess, TError> {
+export function Try<T, E>(cb: () => T): Result<T, E> {
     try {
         return Ok(cb());
     } catch (error) {
