cd833b3ff260ba087e9bf22cab985639e9a3ed7c
Nate Buckareff <nate.buckareff@gmail.com>
2020-01-02 16:05:04 -0500

    Fixes SchemaBuilder to find all named dependencies

--- src/SchemaBuilder.ts
+++ src/SchemaBuilder.ts
@@ -32,22 +32,53 @@ function topologicalSort<T, K>(deps: Map<T, Iterable<T>>, key: (x: T) => K) {
 }
 
 export class SchemaBuilder {
+    private deps = new Map<string, Set<string>>();
     private defs = new Map<string, CodecLike>();
 
     register<C extends CodecLike>(codec: C) {
         if (codec.hasSchemaDefinition()) {
             const exists = this.defs.get(codec.name);
             if (exists !== undefined && exists !== codec) {
+                // Error when trying to re-register a codec with a different name
                 throw new Error(
                     `Codec definition already exists for "${codec.name}"`,
                 );
             } else {
+                // Add the definition for later code generation
                 this.defs.set(codec.name, codec);
             }
-        }
 
-        for (const r of codec.getReferences()) {
-            this.register(r);
+            // Make sure there is at least an empty depdency set so that `generate()` knows
+            // about our codec
+            if (!this.deps.has(codec.name)) {
+                this.deps.set(codec.name, new Set());
+            }
+
+            // Find all dependencies for this codec
+            for (const r of codec.getReferences()) {
+                this.addDependency(codec.name, r);
+            }
+        }
+    }
+
+    private addDependency<C extends CodecLike>(parent: string, codec: C) {
+        if (codec.hasSchemaDefinition()) {
+            this.defs.set(codec.name, codec);
+
+            let s = this.deps.get(parent);
+            if (s === undefined) {
+                s = new Set();
+                this.deps.set(parent, s);
+            }
+            s.add(codec.name);
+
+            for (const r of codec.getReferences()) {
+                this.addDependency(codec.name, r);
+            }
+        } else {
+            for (const r of codec.getReferences()) {
+                this.addDependency(parent, r);
+            }
         }
     }
 
@@ -78,29 +109,17 @@ export class SchemaBuilder {
             definitions: [],
         };
 
-        const deps = new Map<string, Set<string>>();
-        const defs = new Map<string, Reference>();
-
-        for (const [name, codec] of this.defs.entries()) {
-            let s = deps.get(name);
-            if (s === undefined) {
-                s = new Set();
-                deps.set(name, s);
-            }
-            const def = codec.schemaDefinition(c => {
-                if (c.hasSchemaDefinition()) {
-                    s!.add(c.name);
-                }
-            });
-            defs.set(name, def);
+        const refs = new Map<string, Reference>();
+        for (const [name, codec] of this.defs) {
+            refs.set(name, codec.schemaDefinition());
         }
 
-        const sorted = topologicalSort(deps, x => x);
+        const sorted = topologicalSort(this.deps, x => x);
         for (const name of sorted) {
             schema.definitions.push({
                 type: 'CodecDefinition',
                 name,
-                reference: defs.get(name)!,
+                reference: refs.get(name)!,
             });
         }
 
